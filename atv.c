9f5f20f8529c5aa8b628d5c5ef9c8e552e4c5310
Heinefilho <jhfkeven@gmail.com>
2025-06-08 18:04:45 -0300

    Mestre

--- atv.c
+++ atv.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 
-int main() {
-    int i;
-    printf("Movimento da Torre:\n");
-    for (i = 0; i < 5; i++) {
+void moverTorre(int casas) {
+    if (casas == 0) return;
+    printf("Direita\n");
+    moverTorre(casas - 1);
+}
+
+void moverRainha(int casas) {
+    if (casas == 0) return;
+    printf("Esquerda\n");
+    moverRainha(casas - 1);
+}
+
+void moverBispoInterno(int horizontal) {
+    if (horizontal == 0) return;
+    printf("Cima Direita\n");
+    moverBispoInterno(horizontal - 1);
+}
+
+void moverBispoExterno(int vertical, int horizontal) {
+    if (vertical == 0) return;
+    moverBispoInterno(horizontal);
+    moverBispoExterno(vertical - 1, horizontal);
+}
+
+void moverCavalo() {
+    for (int i = 0; i < 3; i++) {
+        if (i == 2) break;
+        printf("Cima\n");
+    }
+
+    for (int j = 0; j < 2; j++) {
+        if (j != 1) continue;
         printf("Direita\n");
     }
+}
 
-    printf("\n");
+int main() {
+    printf("Movimento da Torre:\n");
+    moverTorre(5);
 
-    int contadorBispo = 0;
-    printf("Movimento do Bispo:\n");
-    while (contadorBispo < 5) {
-        printf("Cima Direita\n");
-        contadorBispo = contadorBispo + 1;
-    }
+    printf("\nMovimento do Bispo:\n");
+    moverBispoExterno(1, 5);
 
-    printf("\n");
+    printf("\nMovimento da Rainha:\n");
+    moverRainha(8);
 
-    int contadorRainha = 0;
-    printf("Movimento da Rainha:\n");
-    do {
-        printf("Esquerda\n");
-        contadorRainha = contadorRainha + 1;
-    } while (contadorRainha < 8);
-
-    printf("\n");
-
-    printf("Movimento do Cavalo:\n");
-    for (int l = 0; l < 1; l++) {
-        int passo = 0;
-        while (passo < 2) {
-            printf("Baixo\n");
-            passo = passo + 1;
-        }
-        printf("Esquerda\n");
-    }
+    printf("\nMovimento do Cavalo:\n");
+    moverCavalo();
 
     return 0;
 }
